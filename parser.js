3cf90d3dc54037633c2f4f216ec96adf8b8b9425
tterimaa <tterimaa@gmail.com>
2020-01-19 21:35:26 +0200

    Parse description

--- parser.js
+++ parser.js
@@ -1,15 +1,22 @@
-const parseDeps = depsString => {
-    let deps = depsString.split(', ');
-    let normal = deps.filter(d => !d.includes('|')).map(d => d.slice(0, d.indexOf(' ')));
-    let alt = deps.filter(d => d.includes('|')).map(d => d.split(' | ')).flat().map(d => d.slice(0, d.indexOf(' ')));
+const parseDeps = depsStr => {
+    const deps = depsStr.split(', ');
+    const normal = deps.filter(d => !d.includes('|')).map(d => d.slice(0, d.indexOf(' ')));
+    const alt = deps.filter(d => d.includes('|')).map(d => d.split(' | ')).flat().map(d => d.slice(0, d.indexOf(' ')));
     return { normal, alt };
+}
+
+const parseDescription = elementStr => {
+    const idxMaintainer = elementStr.indexOf('Original-Maintainer:');
+    const idxHomepage = elementStr.indexOf('Homepage:')
+    const lastIndex = idxMaintainer < idxHomepage ? idxMaintainer : idxHomepage;
+    return elementStr.substring(elementStr.indexOf('Description:'), lastIndex);
 }
 
 const parseData = rawData => {
     const array = rawData.split('\n\n');
 
     const parsed = array.reduce((accumulator, current) => {
-    let filtered = current.split('\n').filter(line => line.startsWith('Package') || line.startsWith('Depends') || line.startsWith('Description'));
+    const filtered = current.split('\n').filter(line => line.startsWith('Package') || line.startsWith('Depends'))
         if(filtered.length === 0) return accumulator;
         
         let entry = {};
@@ -18,6 +25,9 @@ const parseData = rawData => {
             let value = keyValue[0] === 'Depends' ? parseDeps(keyValue[1]) : keyValue[1];
             entry[keyValue[0]] = value;
         })
+
+        const description = current.includes('Description') ? parseDescription(current) : null;
+        entry['Description'] = description;
 
         entry.hasOwnProperty('Depends') ? accumulator.push(entry) : accumulator.push({...entry, Depends: { normal: [], alt: []}});
         return accumulator;
