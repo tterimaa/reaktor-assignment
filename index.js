847221b360fe4d2e4471a9a5ef9775976d343845
tterimaa <tterimaa@gmail.com>
2020-01-20 20:06:33 +0200

    Render deps, Clean info when new render occurs.

--- index.js
+++ index.js
@@ -7,19 +7,11 @@ window.onload = function() {
     fetch(baseUrl).then(res => {
         res.json().then(json => {
             data = json;
-            createView(json)
+            console.log(json);
+            renderPackagesList();
+            renderInfo(data[0].Package);
         });
     })
-}
-
-const createView = data => {
-    console.log(data);
-    const packages = document.querySelector('#packages');
-    const ul = document.createElement('ul');
-    data.map(e => {
-        ul.appendChild(newElement('li', e.Package, newListener(e.Package)));
-    })
-    packages.appendChild(ul);
 }
 
 const newListener = packageName => {
@@ -29,11 +21,29 @@ const newListener = packageName => {
     }
 }
 
+const renderPackagesList = () => {
+    const packages = document.querySelector('#packages');
+    const ul = document.createElement('ul');
+    data.map(e => ul.appendChild(newElement('li', e.Package, newListener(e.Package))));
+    packages.appendChild(ul);
+}
+
 const renderInfo = name => {
     const info = document.querySelector('#info');
+    cleanInfo(info);
     const package = data.find(e => e.Package === name);
     info.appendChild(newElement('h1', package.Package));
     info.appendChild(newElement('p', package.Description));
+    const normalDeps = document.createElement('ul');
+    const altDeps = document.createElement('ul');
+    package.Depends.normal.map(dep => normalDeps.appendChild(newElement('li', dep, newListener(dep))));
+    package.Depends.alt.map(dep => altDeps.appendChild(newElement('li', dep, newListener(dep))));
+    info.appendChild(normalDeps);
+    info.appendChild(altDeps);
+}
+
+const cleanInfo = element => {
+    while(element.firstChild) element.removeChild(element.firstChild);
 }
 
 const newElement = (type, content, listener) => {
--- parser.js
+++ parser.js
@@ -9,7 +9,6 @@ const parseDescription = elementStr => {
     const idxMaintainer = elementStr.indexOf('Original-Maintainer:');
     const idxHomepage = elementStr.indexOf('Homepage:');
     const lastIndex = idxMaintainer === -1 && idxHomepage === -1 ? null : idxMaintainer > -1 && idxHomepage > -1 ? Math.min(idxHomepage, idxMaintainer) : Math.max(idxHomepage, idxMaintainer);
-    console.log(lastIndex);
     return lastIndex ? elementStr.substring(elementStr.indexOf('Description: '), lastIndex - 1).replace('Description: ', '') : elementStr.substring(elementStr.indexOf('Description: ')).replace('Description: ', '');
 }
 
