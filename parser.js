039199db3523a275576ebd62d2a98c2317ecf802
tterimaa <tterimaa@gmail.com>
2020-01-20 20:22:31 +0200

    Parser repaired from cutting last letter of deps without whitespace

--- parser.js
+++ parser.js
@@ -1,7 +1,7 @@
 const parseDeps = depsStr => {
     const deps = depsStr.split(', ');
-    const normal = deps.filter(d => !d.includes('|')).map(d => d.slice(0, d.indexOf(' ')));
-    const alt = deps.filter(d => d.includes('|')).map(d => d.split(' | ')).flat().map(d => d.slice(0, d.indexOf(' ')));
+    const normal = deps.filter(d => !d.includes('|')).map(d => d.slice(0, d.indexOf(' ') < 0 ? d.lenght : d.indexOf(' ')));
+    const alt = deps.filter(d => d.includes('|')).map(d => d.split(' | ')).flat().map(d => d.slice(0, d.indexOf(' ') < 0 ? d.lenght : d.indexOf(' ')));
     return { normal, alt };
 }
 
